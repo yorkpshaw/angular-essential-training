ff0080b9c9d617866e9a08637313b6bb57b020bc
York Shaw <york.p.shaw@gmail.com>
2023-09-13 16:15:11 -0500

    Styling Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'mw-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [`
+        h1 { color: #ffffff; }
+        .description {
+            font-style: italic;
+            color: green;
+        }
+    `]
 })
 export class AppComponent {}
