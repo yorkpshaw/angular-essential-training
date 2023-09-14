9c52563a3eed8576fd3f98db7dd243bb2a99f8e4
York Shaw <york.p.shaw@gmail.com>
2023-09-14 16:07:18 -0500

    Removed alias and added @output

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'mw-media-item',
@@ -6,9 +6,11 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
-    @Input('mediaItemToWatch') mediaItem;
+    @Input() mediaItem;
+    @Output() delete = new EventEmitter();
 
     onDelete() {
         console.log('deleted');
+        this.delete.emit(this.mediaItem);
     }
 }
