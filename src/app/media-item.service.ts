0e6d84006e4c64947ab47934094699083fcf1d37
York Shaw <york.p.shaw@gmail.com>
2023-10-09 12:35:23 -0500

    Handling HTTP Errors

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { map } from 'rxjs/operators';
 export class MediaItemService {
   constructor(private http: HttpClient) {}
 
-  get(medium) {
+  get(medium: string) {
     const getOptions = {
       params: { medium }
     };
@@ -16,16 +17,28 @@ export class MediaItemService {
       .pipe(
         map((response: MediaItemsResponse) => {
           return response.mediaItems;
-        })
+        }),
+        catchError(this.handleError)
       );
   }
 
   add(mediaItem: MediaItem) {
-    return this.http.post('mediaitems', mediaItem);
+    return this.http.post('mediaitems', mediaItem)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   delete(mediaItem: MediaItem) {
-    return this.http.delete(`mediaitems/${mediaItem.id}`);
+    return this.http.delete(`mediaitems/${mediaItem.id}`)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error(error.message);
+    return throwError('A data error occurred, please try again.');
   }
 }
 
