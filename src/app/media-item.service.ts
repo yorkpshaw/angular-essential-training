21d6e97677f0e3d242096857f45853b9119d6528
York Shaw <york.p.shaw@gmail.com>
2023-10-09 12:01:54 -0500

    HTTP Client Get Calls

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaItemService {
+  constructor(private http: HttpClient) {}
+
   mediaItems = [
     {
       id: 1,
@@ -50,7 +54,12 @@ export class MediaItemService {
   ];
 
   get() {
-    return this.mediaItems;
+    return this.http.get<MediaItemsResponse>('mediaitems')
+      .pipe(
+        map((response: MediaItemsResponse) => {
+          return response.mediaItems;
+        })
+      );
   }
 
   add(mediaItem) {
@@ -63,4 +72,18 @@ export class MediaItemService {
       this.mediaItems.splice(index, 1);
     }
   }
+}
+
+interface MediaItemsResponse {
+  mediaItems: MediaItem[];
+}
+
+export interface MediaItem {
+  id: number;
+  name: string;
+  medium: string;
+  category: string;
+  year: number;
+  watchedOn: number;
+  isFavorite: boolean;
 }
